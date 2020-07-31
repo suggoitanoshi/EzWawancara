1a3d5a4d2264d7f7d6a20ef541dccffb599a5c67
AlfandaviAU <alfanau18@gmail.com>
2020-07-31 22:09:55 +0700

    First commit

---
+++ wawan.py
@@ -0,0 +1,32 @@
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial",size=20)
+# Settingan font dan size disini
+
+print("""
+##########################
+### Ez Data Wawancara  ###
+##########################
+""")
+
+NimAnda = int(input("Masukkan nim kamu : "))
+Nama    = str(input("Nama              : "))
+Nim     = int(input("NIM               : "))
+Jurusan = str(input("Jurusan           : "))
+Unik    = str(input("Keunikan          : "))
+Fi      = str(input("First Impression  : "))
+# Input datanya
+
+pdf.cell(200,10,txt = "Nama : %s" %Nama,ln=1)
+pdf.cell(200,10,txt = "Nim : %d" %Nim,ln=2)
+pdf.cell(200,10,txt = "Jurusan : %s" %Jurusan,ln=3)
+pdf.cell(200,10,txt = "Keunikan : %s" %Unik,ln=4)
+pdf.cell(200,10,txt = "First Impression : %s" %Fi,ln=5)
+# Prosesnya
+
+out = str(NimAnda)+"_"+str(Nim)+".pdf"
+pdf.output(out)
+# Hasilnya
+
